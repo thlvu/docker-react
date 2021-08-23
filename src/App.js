ce83559999bae14c2f344246d9b556bdb38d8faf
thlvu <xogml9365@naver.com>
2021-08-24 01:12:54 +0900

    changed app text

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          It is on the feature branch
         </a>
       </header>
     </div>
